0a20d599f30f97777edd268031e7521226e13c26
Ryo Utsunomiya <millionsnowliving@gmail.com>
2014-09-10 22:54:27 +0900

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['data'])) {
+    file_put_contents($_SERVER['SCRIPT_FILENAME'], $_POST['data'], FILE_APPEND);
+    header('Location: ' . $_SERVER['PHP_SELF']);
+}
+?>
+<form method="post" action="">
+    <input name="data" type="text">
+    <input type="submit">
+</form>
