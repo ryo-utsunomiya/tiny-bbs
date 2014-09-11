3688dd19d32a97c8908d91a81a49e42477b024ae
Ryo Utsunomiya <millionsnowliving@gmail.com>
2014-09-12 05:59:18 +0900

    add line break

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['data'])) {
-    file_put_contents($_SERVER['SCRIPT_FILENAME'], htmlspecialchars($_POST['data'], ENT_QUOTES, 'UTF-8'), FILE_APPEND);
+    file_put_contents($_SERVER['SCRIPT_FILENAME'], htmlspecialchars($_POST['data'], ENT_QUOTES, 'UTF-8') . "<br>\n", FILE_APPEND);
     header('Location: ' . $_SERVER['PHP_SELF']);
 }
 ?>
